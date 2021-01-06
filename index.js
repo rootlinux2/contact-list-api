2efa5caac12832d44ff48cf72650d44a36457865
Yoandry Collazo <ycbarrosocu@gmail.com>
2021-01-06 14:04:41 -0500

    👌 IMPROVE: Server Configurations

--- index.js
+++ index.js
@@ -2,14 +2,63 @@ const express = require('express');
 import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { errorHandler } from './src/utils';
+import routes from './src/routes/routes';
 
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const port = 3000;
-app.get('/', (req, res) => {
- res.status(200).send(`Hello World! Our server is running at port ${port}`);
-});
-server.listen(port, () => {
- console.log(`Server running at port ${port}`);
+const PORT = 3002;
+const url = 'mongodb://localhost:27017/contacts';
+
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoIndex: false,
+    poolSize: 10,
+    useUnifiedTopology: true,
+    bufferMaxEntries: 0,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4,
+    keepAlive: true,
+    keepAliveInitialDelay: 300000
+  };
+  mongoose.Promise = global.Promise;
+  mongoose.connect(url, options);
+
+  // mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose default connection open to ${url}`);
+  });
+  
+  // If the connection throws an error
+  mongoose.connection.on('error', err => {
+    console.log(`Mongoose default connection error: ${err}`);
+  });
+  
+  // When the connection is disconnected
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose default connection disconnected');
+  });
+  
+  // If the Node process ends, close the Mongoose connection
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+    });
+  });
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
+
+routes(app);
+
+app.use(errorHandler);
+
+const server = require('http').createServer(app);
+
+server.listen(process.env.PORT || PORT, () => {
+  console.log(`you are server is running on ${PORT}`);
 });
